bbeeb31f8140a52f23c06f8b22e72c7192c8834e
peaceful-cyborg-2337 <cmajewski@wesleyan.edu>
2019-07-12 22:03:29 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -27,9 +27,9 @@ def using_concat(array1,array2)
 
 end
 
-def using_insert(array,new_element)
-
-  array.insert(3,new_element)
+def using_insert(array,element)
+  
+  array.insert(3,element)
 
 end
 
